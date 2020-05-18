fae6e21406ed0bc2aefd736de6f073db351e0565
Irina Muchnik <irina.muchnik@cfpb.gov>
2020-05-18 13:43:37 -0400

    testing on orsee

--- vars/prUtils.groovy
+++
@@ -1,9 +0,0 @@
-
-import groovy.json.JsonSlurper
-
-def call(prs) {
-    def closedPRs = new JsonSlurper().parseText(prs).collect { pr ->
-        return pr.state == 'closed'
-    }
-    closedPRs
-}
